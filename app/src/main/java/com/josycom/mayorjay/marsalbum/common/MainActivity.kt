e73c315c206fcbdfbc2e542e6213ce021ad1c1fa
mayorjay <joseolu4gsm@yahoo.com>
2023-03-24 20:51:29 +0100

    feat: Implement SplashScreen

--- app/src/main/java/com/josycom/mayorjay/marsalbum/common/MainActivity.kt
+++ app/src/main/java/com/josycom/mayorjay/marsalbum/common/MainActivity.kt
@@ -2,12 +2,14 @@ package com.josycom.mayorjay.marsalbum.common
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.josycom.mayorjay.marsalbum.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
